fe6de6c41b9c62fc119e2eb1ceaae7e92bba3ae3
MatheusErcoli <matheusercoli@gmail.com>
2026-07-30 18:36:17 -0300

    feat: configurando o Umzug

---
+++ backend/src/config/database/migrator.ts
@@ -0,0 +1,19 @@
+import { Umzug, SequelizeStorage } from "umzug";
+import sequelize from "../database";
+
+export const migrator = new Umzug({
+  migrations: {
+    glob: "src/database/migrations/*.ts",
+  },
+  context: sequelize.getQueryInterface(),
+  storage: new SequelizeStorage({ sequelize }),
+  logger: console,
+});
+
+export async function migrate() {
+  await migrator.up();
+}
+
+export async function rollback() {
+  await migrator.down();
+}

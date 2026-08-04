a1e4fe8dc5081fbe70593340457ae7e004f168e5
MatheusErcoli <matheusercoli@gmail.com>
2026-08-03 22:24:20 -0300

    feat: tratamento de erros

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+import { errorMiddleware } from './middlewares/error.middleware';
 
 const app = express();
 
@@ -9,8 +11,10 @@ app.use(cors());
 
 app.use(helmet());
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 app.use(express.json());
+
+app.use(errorMiddleware);
 
 export default app;
